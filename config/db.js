7a96dc4ee20ee32d09a89b57c13801deee3a2509
Brooke <bsm71@uclive.ac.nz>
2021-03-17 10:48:58 +1300

    setting up sql

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 let pool = null;
 
 exports.createPool = async function () {
-    pool = mysql.createPool({
+    pool = await mysql.createPool({
         multipleStatements: true,
         host: process.env.SENG365_MYSQL_HOST,
         user: process.env.SENG365_MYSQL_USER,
@@ -11,6 +12,8 @@ exports.createPool = async function () {
         database: process.env.SENG365_MYSQL_DATABASE,
         port: process.env.SENG365_MYSQL_PORT || 3306
     });
+    await pool.getConnection(); //Check connection
+    console.log('Successfully connected to the database');
 };
 
 exports.getPool = function () {
